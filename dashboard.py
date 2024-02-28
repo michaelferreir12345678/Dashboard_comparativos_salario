b2cb3886787b1f96f894aeb0aab268a9263cdd0f
michael12345678 <michaelferreiradesousa@gmail.com>
2024-02-28 15:36:30 -0300

    Feito alterações no script com inclusão da nova planilha com valores da AMC

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 # Função para carregar os dados do Excel
 def carregar_dados():
-    return pd.read_excel("salary_data.xlsx", sheet_name="salary_data")
+    return pd.read_excel("planilha_impacto_salarial.xlsx", sheet_name="amc")
 
 # Função para substituir o ponto pela vírgula nos valores do DataFrame
 def substituir_ponto_por_virgula(df):
@@ -14,34 +13,37 @@ def substituir_ponto_por_virgula(df):
 
 # Função para calcular a média salarial de cada cargo
 def calcular_media_salarial(df):
-    return df.groupby('cargo')['salario_atual'].mean()
+    return df.groupby('Cargo')['VENCIMENTO BASE'].mean()
 
 # Função para criar e exibir a tabela de salários por classe e referência
 def exibir_tabela_salarios(TC, TR, num_classes, num_referencias, salario_base, nome_tabela):
-    
-    # st.subheader(nome_tabela)
-    
     # Criando um DataFrame para armazenar os resultados
     tabela = pd.DataFrame(index=range(1, num_referencias + 1), columns=range(1, num_classes + 1))
-
+    valores = {}  # Dicionário para armazenar os valores
+    
     # Preenchendo a tabela com os valores de salário
     for j in range(1, num_classes + 1):
         for i in range(1, num_referencias + 1):
             if i == 1 and j == 1:
-                tabela.loc[i, j] = salario_base
+                valor = salario_base
             elif i == 1:
-                tabela.loc[i, j] = tabela.loc[num_referencias, j - 1] * TC
+                valor = float(tabela.loc[num_referencias, j - 1]) * TC
             else:
-                tabela.loc[i, j] = tabela.loc[i - 1, j] * TR
-
+                valor = float(tabela.loc[i - 1, j]) * TR
+            
+            # Armazenando o valor no dicionário
+            valores[(i, j)] = valor, i + (j - 1) * num_referencias
+            tabela.loc[i, j] = f"{valor:.2f}"  # Atribuindo o índice da célula
+                        
     # Renomeando índices e colunas
     tabela.index.name = 'Referência'
     tabela.columns.name = 'Classe'
-
-    return tabela
-
-def main():
     
+    return tabela, valores
+
+
+    
+def main():
     st.header(' :orange[Prefeitura de Fortaleza] ', divider='rainbow'   )
     col1 = st.columns(1)
     col2, col3 = st.columns(2)    
@@ -53,17 +55,13 @@ def main():
     st.sidebar.image('logo.png', width=150, use_column_width=True)
 
     # Exibir filtro de cargo no painel lateral
-    cargo_selecionado = st.sidebar.selectbox('Selecione um cargo:', df['cargo'].unique())
+    cargo_selecionado = st.sidebar.selectbox('Selecione um cargo:', df['Cargo'].unique())
     
     # Opção para escolher qual tabela será exibida
     tabela_selecionada = st.sidebar.selectbox('Selecione a tabela:', ['Tabela B - 180h', 'Tabela C - 180h', 'Tabela D - 180h'])
 
     # Calcular média salarial de cada cargo
     media_salarial_cargos = calcular_media_salarial(df)
-
-    # # Exibir média salarial de cada cargo
-    # st.sidebar.subheader('Média Salarial de Cada Cargo')
-    # st.sidebar.write(media_salarial_cargos)
 
     # Parâmetros Tabela 1
     st.sidebar.subheader('Tabela Personalizável')
@@ -89,21 +87,30 @@ def main():
     salario_base_d = 1582.67
 
     # Exibir e atualizar a tabela de salários por classe e referência (Tabela 1)
-    tabela_salarios1 = col2.dataframe(exibir_tabela_salarios(TC1, TR1, num_classes1, num_referencias1, salario_base1, 'Tabela personalizável'), use_container_width=True)
+    tabela_salarios1, valores1 = exibir_tabela_salarios(TC1, TR1, num_classes1, num_referencias1, salario_base1, 'Tabela personalizável')
+    tabela_salarios1 = col2.dataframe(tabela_salarios1, use_container_width=True)
+    
+    indice_desejado = 6 # Índice desejado
 
-
-    # tabela_salarios_b = exibir_tabela_salarios(1.05, 1.02, num_classes_b, num_referencias_b, salario_base_b, 'B')
+    # Verificar se o índice está presente no dicionário
+    # if (indice_desejado, 1) in valores1:
+    #     valor_desejado = valores1[(indice_desejado, 1)][0]
+    #     return valor_desejado
+    
     # Exibir e atualizar a tabela de salários por classe e referência (Tabela B)
     if tabela_selecionada == 'Tabela B - 180h':
-        col3.dataframe(exibir_tabela_salarios(1.05, 1.02, num_classes_b, num_referencias_b, salario_base_b, 'Tabela B - 180h'),use_container_width=True)
+        tabela_salarios_b, valores_b = exibir_tabela_salarios(1.05, 1.02, num_classes_b, num_referencias_b, salario_base_b, 'Tabela B - 180h')
+        col3.dataframe(tabela_salarios_b, use_container_width=True)
+
     # Exibir e atualizar a tabela de salários por classe e referência (Tabela C)
     if tabela_selecionada == 'Tabela C - 180h':
-        st.subheader("tabela C - 180h")
-        col3.dataframe(exibir_tabela_salarios(1.05, 1.02, num_classes_c, num_referencias_c, salario_base_c, 'Tabela C - 180h'), use_container_width=True)
+        tabela_salarios_c, valores_c = exibir_tabela_salarios(1.05, 1.02, num_classes_c, num_referencias_c, salario_base_c, 'Tabela C - 180h')
+        col3.dataframe(tabela_salarios_c, use_container_width=True)
 
     # Exibir e atualizar a tabela de salários por classe e referência (Tabela D)
     if tabela_selecionada == 'Tabela D - 180h':
-        col3.dataframe(exibir_tabela_salarios(1.05, 1.02, num_classes_d, num_referencias_d, salario_base_d, 'Tabela D - 180h'), use_container_width=True)
+        tabela_salarios_d, valores_d = exibir_tabela_salarios(1.05, 1.02, num_classes_d, num_referencias_d, salario_base_d, 'Tabela D - 180h')
+        col3.dataframe(tabela_salarios_d, use_container_width=True)
 
 if __name__ == '__main__':
     main()
